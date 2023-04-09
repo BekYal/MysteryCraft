50ee022cd40b9edd0e39efdb224c2f071e70e912
hygohy@gmail.com <hygohy@gmail.com>
2023-04-09 16:04:04 +0300

    create belt and rubt modificator

--- core/dev/Header/MysteryAPI.js
+++ core/dev/Header/MysteryAPI.js
@@ -8,3 +8,15 @@ var
 	ParticleType = Native.ParticleType,
 	BlockSide = Native.BlockSide,
 	EntityType = Native.EntityType;
+
+
+
+
+IDRegistry.genItemID('ruby');
+IDRegistry.genItemID('Empty_belt');
+
+	Item.createItem("ruby", "ruby",
+		{ name: "Ruby_modificator" }, { stack: 1 });
+		
+	Item.createItem("Empty_belt", "Empty_belt", 
+		{ name: "Empty_belt" }, { stack: 1 });
