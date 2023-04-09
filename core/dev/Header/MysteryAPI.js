e50d213b63565301a5af2a921795430ca1164359
hygohy@gmail.com <hygohy@gmail.com>
2023-04-09 16:02:39 +0300

    import lib and headers

---
+++ core/dev/Header/MysteryAPI.js
@@ -0,0 +1,10 @@
+IMPORT('EnchantsHelper');
+var
+	EntityFeatures = {},
+	ImprovedItems = {},
+	MysteryAPI = {},
+	Color = android.graphics.Color,
+	PotionEffect = Native.PotionEffect,
+	ParticleType = Native.ParticleType,
+	BlockSide = Native.BlockSide,
+	EntityType = Native.EntityType;
