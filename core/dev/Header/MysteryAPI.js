62cd7179de928126ba5e54873cd08a2f62e448bd
hygohy@gmail.com <hygohy@gmail.com>
2023-04-09 16:11:40 +0300

    add header func

--- core/dev/Header/MysteryAPI.js
+++ core/dev/Header/MysteryAPI.js
@@ -10,6 +10,16 @@ var
 	EntityType = Native.EntityType;
 
 
+function transformString(str) {
+	var words = str.split(/(?=[A-Z])/);
+	for (var i = 0; i < words.length; i++) {
+		words[i] = words[i].toLowerCase();
+		if (i > 0) {
+			words[i] = ' ' + words[i];
+		}
+	}
+	return words.join('');
+}
 
 
 IDRegistry.genItemID('ruby');
