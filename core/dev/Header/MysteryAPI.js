c13d3f363ebd348f77df8af2fd65583d34195e43
hygohy@gmail.com <hygohy@gmail.com>
2023-05-14 17:04:38 +0300

    я хуй знает чо сделал, но чото сделал

--- core/dev/Header/MysteryAPI.js
+++ core/dev/Header/MysteryAPI.js
@@ -1,6 +1,12 @@
 IMPORT('EnchantsHelper');
-var
-	EntityFeatures = {},
+IDRegistry.genBlockID("teerch")
+Block.createBlock("teerch",
+[{ name: "big chest", texture: [["sss", 0], ["sss", 0], ["sss", 0], ["ppp", 0], ["sss", 0], ["sss", 0]], inCreative: true }], {
+	rendertype: 2,
+	renderlayer: 2,
+});
+
+var EntityFeatures = {},
 	ImprovedItems = {},
 	MysteryAPI = {},
 	Color = android.graphics.Color,
@@ -33,25 +39,13 @@ IDRegistry.genItemID('Empty_belt');
 
 
 	
-var ImprovedItem = function(name) {
-	
-	this.createTotem = function(texture, danage, useDecrease, func) {
+var ImprovedItem = function() {
+	this.createTotem = function(name, texture, damage, useDecrease, func) {
 		IDRegistry.genItemID(name);
 		Item.createItem(name, transformString(name), { name: texture }, { stack: 1 });
-			Item.setMaxDamage(ItemID[name], damage)		
+			Item.setMaxDamage(ItemID[name], damage);
 			Item.setAllowedInOffhand(name, true);
-		
-		if(func.tick){
-			Callback.addCallback("ServerPlayerTick", function(player) {
-				let item = Entity.getOffhandItem(player),
-				let	actor = new PlayerActor(player);
-				if (item.id == ItemID[name] && World.getThreadTime() % 40 == 0 ) {
-					func.tick(item, player);
-					actor.setInventorySlot(item, ItemID[name], item.count, item.data =+ useDecrease || 1 , item.extra);
-				}
-			});
-		}
-		if(func.hurt){
+		if (func.hurt){
 				Callback.addCallback('EntityHurt', function(attacker, victim, damageValue, damageType, someBool0, someBool2) {
 				let item = Entity.getOffhandItem(attacker);
 				let	actor = new PlayerActor(player);
@@ -61,7 +55,27 @@ var ImprovedItem = function(name) {
 				}
 			});
 		}
-		if (ownenrHurt) {
+		if (func.tick){
+			Callback.addCallback("ServerPlayerTick", function(player) {
+				let item = Entity.getOffhandItem(player),
+					actor = new PlayerActor(player)
+				if (item.id == ItemID[name] && World.getThreadTime() % 40 == 0 ) {
+					func.tick(item, player);
+					actor.setInventorySlot(item, ItemID[name], item.count, item.data =+ useDecrease || 1 , item.extra);
+				}
+			});
+		}
+		if (func.hurt){
+				Callback.addCallback('EntityHurt', function(attacker, victim, damageValue, damageType, someBool0, someBool2) {
+				let item = Entity.getOffhandItem(attacker);
+				let	actor = new PlayerActor(player);
+				if (item.id == ItemID[name]) {
+					func.hurt(item, attacker, victim, damageValue, damageType);
+					actor.setInventorySlot(item, ItemID[name], item.count, item.data =+ useDecrease || 1 , item.extra);
+				}
+			});
+		}
+		if (func.ownenrHurt) {
 			Callback.addCallback('EntityHurt', function(attacker, victim, damageValue, damageType, someBool0, someBool2) {
 				let item = Entity.getOffhandItem(victim);
 				let	actor = new PlayerActor(player);
@@ -72,14 +86,12 @@ var ImprovedItem = function(name) {
 			});
 		}
 		if (func.destroyBlock) {
-				Callback.addCallback("DestroyBlock", function (coords, block, player){
-				let 
-				   	region = BlockSource.getDefaultForActor(player),
-				   	item = Entity.getOffhandItem(player);
-				let	actor = new PlayerActor(player);
+			Callback.addCallback("DestroyBlock", function (coords, block, player){
+				let region = BlockSource.getDefaultForActor(player),
+					item = Entity.getOffhandItem(player), 
+					actor = new PlayerActor(player);
 				if (item.id == ItemID[name]) {
-					let 
-						enchants = item.extra.getEnchants(), 
+					let enchants = item.extra.getEnchants(), 
 						enchantData = {
 							efficiency: enchants[EEnchantment.EFFICIENCY],
 							experience: 0, //hz
@@ -87,103 +99,20 @@ var ImprovedItem = function(name) {
 							silk: enchants[EEnchantment.SILK],
 							unbreaking: enchants[EEnchantment.UNBREAKING]
 						};
+					
 					func(item, enchantData, region, coods, block, player);
 					actor.setInventorySlot(item, ItemID[name], item.count, item.data =+ useDecrease || 1 , item.extra);
 			    }
 			});
 		}
-	};
+	}
 }
 
-ImprovedItems.createTotem = function(name, texture, type, func, par) {
-	IDRegistry.genItemID(name);
-	Item.createItem(name, transformString(name), { name: texture }, { stack: 1 });
-	Item.setAllowedInOffhand(name, true);
-	switch (type) {
-		case 'tick':
-			Callback.addCallback("ServerPlayerTick", function(player) {
-				let item = Entity.getOffhandItem(player);
-				if (item.id == ItemID[name]) {
-					func(item, player);
-				}
-			});
-			break;
-		case 'hurt':
-			Callback.addCallback('EntityHurt', function(attacker, victim, damageValue, damageType, someBool0, someBool2) {
-				let item = Entity.getOffhandItem(attacker);
-				if (item.id == ItemID[name]) {
-					func(item, attacker, victim, damageValue, damageType);
-				}
-			});
-			break;
-		case 'userHurt':
-			Callback.addCallback('EntityHurt', function(attacker, victim, damageValue, damageType, someBool0, someBool2) {
-				let item = Entity.getOffhandItem(victim);
-				if (item.id == ItemID[name]) {
-					func(item, attacker, victim, damageValue, damageType);
-				}
-			});
-			break;
-		case 'destroyBlock':
-			Callback.addCallback("DestroyBlock", function (coords, block, player){
-				let 
-					region = BlockSource.getDefaultForActor(player),
-					item = Entity.getOffhandItem(player);
-				if (item.id == ItemID[name]) {
-					let 
-						enchants = item.extra.getEnchants(), 
-						enchantData = {
-							efficiency: enchants[EEnchantment.EFFICIENCY],
-							experience: 0, //hz
-							fortune: enchants[EEnchantment.FORTUNE],
-							silk: enchants[EEnchantment.SILK],
-							unbreaking: enchants[EEnchantment.UNBREAKING]
-						};
-					func(item, enchantData, region, coods, block, player)
-			    }
-			});
-	}
-	if (par) {
-		Item.setEnchantType(ItemID[name], Mask.all, par.enchanted);
-	}
-};
+var hu = new ImprovedItem()
+	.createTotem('huyaaaaaaaaaa', 'stick', 10, 3, {
+		tick: function(item, player){
+			Game.message("hh");
+		}
+	})
 
 
-
-
-
-ImprovedItems.createTotemq = function(name, texture){
-	IDRegistry.genItemID(name);
-	Item.createItem(name, transformString(name), { name: texture }, { stack: 1 });
-	Item.setAllowedInOffhand(name, true);	
-};
-
-
-
-
-ImprovedItems.createTotemq('debugTotem', 'totem');
-
-Callback.addCallback('EntityHurt', function (attacker, victim, damageValue, damageType, someBool1, someBool2) {
-   let item = Entity.getOffhandItem(Player.get()); 
-   if (item.id == ItemID.debugTotem) {
-   if (victim == Player.get() ) 
-	 Game.tipMessage("damageValue: " + damageValue + '\ndamageType: ' + damageType)
-		  else	Game.tipMessage("damageValue: " + damageValue + '\ndamageType: ' + damageType + '\nvictim: ' + victim);
-   }
-});
-
-
-
-
-ImprovedItems.createTotem('EnchantedTotem', 'totem', 'destroyBlock', 
-	function(item, enchantData, region, coods, block, player){
-		Game.prevent();
-	let funct = Block.getDropFunction(block.id),
-		enchants = item.extra.getEnchants(), 
-		drops = funct(coords, block.id, block.data, ToolAPI.getBlockData(block.id).level, enchantData, Entity.getCarriedItem(player), region);
-		
-	for(let i in drops)
-		        region.spawnDroppedItem(coords.x + .5, coords.y + .5, coords.z + .5, drops[i].id, drops[i].count, drops[i].data, drops[i].extra);
-		    region.destroyBlock(coords.x, coords.y, coords.z);
-	}
-);
